09e393a366ad03bf6be42c8c3e6cc72eba3c80e4
ajinkode <ajinkode@gmail.com>
2023-09-14 22:55:17 +0530

    commit

--- Band Generator Project/index.js
+++ Band Generator Project/index.js
@@ -5,13 +5,6 @@ const app = express();
 const port = 3000;
 
 
-//hello
-//hello
-//hello
-//hello
-//hello
-//hello
-
 //Step 3 - Make the styling show up.
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
